e2aa3aab602d713764179fb32b4b54a681ce3e7f
Nekaque <3217cody@gmail.com>
2026-05-14 14:29:58 +0200

    multi.py mega nice

---
+++ multi_init.py
@@ -0,0 +1,13 @@
+import sys
+import subprocess
+from setup import Options
+import json
+
+if __name__ == "__main__":
+    opt = Options()
+    opt.select_device(0)
+    opt.select_model(0)
+    payload_device = json.dumps(opt.selected_device)
+    # payload_messages = json.dumps(messages)
+
+    subprocess.run([sys.executable, "multi.py", payload_device, opt.model()])
--- temp.py
+++ temp.py
@@ -1,24 +1,30 @@
+import json
 import re
 import subprocess
 import sys
 
-print("🔍 Scanning hardware... (this takes a second)")
-script = f"""
-import sys
-from llama_cpp import Llama
-try:
-   llm = Llama(model_path='models/gemma-4-E2B-it-Q4_K_M.gguf', n_gpu_layers=1, verbose=True)
-except Exception:
-   pass
-"""
-result = subprocess.run([sys.executable, "-c", script], capture_output=True, text=True, encoding='utf-8')
-devices = []
-print(result.stderr)
-for line in result.stderr.split('\n'):
-    match = re.search(r"ggml_vulkan:\s+(\d+)\s+=\s+(.*?)\s+\|", line)
-    if match:
-        print(line)
-        devices.append({"id": match.group(1), "name": match.group(2).strip(), "type": "Vulkan"})
-
-cpu_id = str(len(devices))
-devices.append({"id": cpu_id, "name": "CPU", "type": "CPU"})
+# print("🔍 Scanning hardware... (this takes a second)")
+# script = f"""
+# import sys
+# from llama_cpp import Llama
+# try:
+#    llm = Llama(model_path='models/gemma-4-E2B-it-Q4_K_M.gguf', n_gpu_layers=1, verbose=True)
+# except Exception:
+#    pass
+# """
+# result = subprocess.run([sys.executable, "-c", script], capture_output=True, text=True, encoding='utf-8')
+# devices = []
+# print(result.stderr)
+# for line in result.stderr.split('\n'):
+#     match = re.search(r"ggml_vulkan:\s+(\d+)\s+=\s+(.*?)\s+\|", line)
+#     if match:
+#         print(line)
+#         devices.append({"id": match.group(1), "name": match.group(2).strip(), "type": "Vulkan"})
+#
+# cpu_id = str(len(devices))
+# devices.append({"id": cpu_id, "name": "CPU", "type": "CPU"})
+with open("messages.json", "r") as f:
+    prompts = json.load(f)
+messages = [{"test_name":"inference speed", "test_id":"nic", "prompts":prompts}]
+with open("mess.json", "w", encoding="utf-8") as f:
+    json.dump(messages, f, indent=4)
